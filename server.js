83914cf76b3411e09f03f15a22ef47a8e9f86a0b
Kamil Makoś <makos.kamil@o2.pl>
2018-07-04 18:06:38 +0200

    upp_serv

--- server.js
+++ server.js
@@ -2,17 +2,18 @@ var express = require('express');
 var app = express();
 app.use(express.static('assets'));
 
-app.get('/:id', function(req, res) {
-    res.sendFile('/index.html');
+app.use(function(req, res, next){
+    console.log('Hej, jestem pośrednikiem między żądaniem a odpowiedzią!');
+    next();
 });
 
-app.get('/userform', function (req, res) {
-    const response = {
-        first_name: req.query.first_name,
-        last_name: req.query.last_name
-    };
-    res.end(JSON.stringify(response));
-})
+app.get('/', function(req, res) {
+    res.send('Hello World');
+});
+
+app.get('/store', function (req, res) {
+    res.send('To jest sklep');
+});
 
 var server = app.listen(3000, 'localhost', function() {
     var host = server.address().address;
